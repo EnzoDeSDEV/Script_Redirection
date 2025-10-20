44324840e8550d0da6548deeb7621f7d1badb1c0
EnzoDeSDEV <desousaenzo0718@gmail.com>
2025-10-20 17:07:14 +0200

    Ajout d'un nouveau script en cas de multiple redirection

---
+++ generation_htaccess_lien_multiple.py
@@ -0,0 +1,15 @@
+import csv
+
+with open('test.csv', 'r') as csvfile:
+    page = []
+    redirection= []
+    delimiter = ";"
+    lecteur = csv.DictReader(csvfile, delimiter = ";")
+    for ligne in lecteur:
+           page.append(ligne['Page'])
+           redirection.append(ligne['Redirections'])
+    with open('rewrite_rules.txt', 'w', encoding='utf-8') as sortie:
+        for url,redir in zip(page, redirection):
+                    if url.startswith('espace-abonnement.lavoixdunord.fr/'):
+                          url = url[len('espace-abonnement.lavoixdunord.fr'):]
+                          sortie.write(f"RewriteRule ^{url}$ {redir} [R=307,QSA,L]\n")
